4eb10f588be638240843a879dda674c6ba99b172
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-01-01 18:44:53 -0600

    keep lening

--- try.py
+++ try.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-data = np.random.uniform(0, 1, 25)
 
-data = np.where(data >= .5, data, 0)
+palabras1 = ["holas", "claro", "super", "huevo", "canasta"]
+palabras2 = ["barco", "viento", "iguana", "agua", "caldos"]
 
-print(data)
+palabras1 = [x for i in palabras1 for x in i]
 
-
+comp = [a + b for a in [x for i in palabras1 for x in i] for b in [y for j in palabras2 for y in j]]
+print(comp)
