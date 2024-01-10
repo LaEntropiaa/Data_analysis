31ad1387306d39acaeffd8e5928d48b267ba9e3b
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-01-10 17:31:26 -0600

    st

--- try.py
+++ try.py
@@ -1,15 +1,16 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 
-titanic_data = pd.read_csv(r"data_sets\titanic_data.csv")
+diamond_data = pd.read_csv(r"data_sets\diamonds.csv")
+del diamond_data["Unnamed: 0"]
+diamond_data["cut"] = diamond_data["cut"].map(
+    {"Ideal":5, "Premium":4, "Very Good":3, "Good":2, "Fair":1}
+    )
+diamond_data["clarity"] = diamond_data["clarity"].map(
+    {"IF":8, "VVS1":7, "VVS2":6, "VS1":5, "VS2":4, "SI1":3, "SI2":2, "I1":1}
+    )
 
-cabin_data = titanic_data["Cabin"].astype(str)
-cabin_data = np.array([cabin[0] for cabin in cabin_data])
-titanic_data["Cabin"] = pd.Categorical(cabin_data)
-
-tab = pd.crosstab(index=titanic_data["Survived"], columns=titanic_data["Pclass"], margins=True, normalize="columns")
-tab.index = ["died", "survived"]
-tab.columns = ["class1", "class2", "class3", "Total"]
-
-print(tab)
+diamond_data["carat"].plot(kind="density")
+plt.show()
