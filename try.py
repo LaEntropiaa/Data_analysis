6e2af310e82837fed67c95ab7b8f3f43bc4428fd
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-01-16 21:25:21 -0600

    WOrking on linked lists, don't know why but it's fun

--- try.py
+++ try.py
@@ -18,24 +18,49 @@ class LinkedList:
         last_node.next = new_node
     
     def pre_append(self, data):
-        new_node = data
+        new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
     
-    def insert(data, index):
-        new_node = data
-        
+    def insert(self, data, index):
+        new_node = Node(data)
+        current_node = self.head
+        current_index = 0
+        while index != current_index:
+            current_node = current_node.next
+            current_index += 1
+        next_node = current_node.next
+        current_node.next = new_node
+        new_node.next = next_node
+
+    def get(self, index):
+        current_node = self.head
+        current_index = 0
+        while index != current_index:
+            try:
+                current_node = current_node.next
+            except AttributeError:
+                return None
+            current_index += 1
+        try:
+            return current_node.data
+        except AttributeError:
+            return None
 
     def show_values(self):
         current_node = self.head
         print("[", end="")
-        while current_node.next:
+        while True:
             print(f"{current_node.data} -> ", end="")
             current_node = current_node.next
+            if current_node.next is None:
+                print(f"{current_node.data} -> ", end="")
+                break
         print("None]")
 
 lista = LinkedList()
 lista.append(1)
 lista.append(2)
-lista.show_values()
+lista.append(3)
+print(lista.get(-1))
 
