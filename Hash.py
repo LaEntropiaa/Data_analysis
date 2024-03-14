2a9e5c430a9b3d3bdfce0635a9c5404c4d3600d4
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-03-14 15:10:20 -0600

    added __str__ method

--- Hash.py
+++ Hash.py
@@ -6,6 +6,15 @@ class Hash:
         self.size = size
         self.array = [None for _ in range(self.size)]
         self.items = 0
+
+    def __str__(self):
+        string = "{"
+        for i in self.array:
+            if i is None:
+                continue
+            string = f"{string} {i[0]}:{i[1]},"
+        string = f"{string[:-1]} {'}'}"
+        return string
         
     def __hash(self, key) -> int:
         key = hash(key)
@@ -17,16 +26,16 @@ class Hash:
             raise KeyError("HashTable out of space")
         position = self.__hash(key)
         while self.array[position] is not None:
-            if self.array[position][1] == key:
-                self.array[position][0] = data
+            if self.array[position][0] == key:
+                self.array[position][1] = data
                 return
             position = (position + 1) % self.size
-        self.array[position] = (data, key)
+        self.array[position] = (key, data)
     
     def get(self, key):
         position = self.__hash(key)
         while self.array[position] is not None:
-            if self.array[position][1] == key:
-                return self.array[position][0]
+            if self.array[position][0] == key:
+                return self.array[position][1]
             position = (position + 1) % self.size
         raise KeyError("Key not found")
