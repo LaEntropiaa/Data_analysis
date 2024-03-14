1d3154bf19ca277c78dbe24c46ab090788eec93d
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-03-13 19:35:11 -0600

    trying

--- try.py
+++ try.py
@@ -1,4 +1,8 @@
 import Hash
 
-var = Hash.Hash(20)
-print(len(var))
+var = Hash.Hash(40)
+var.add(28, "culo")
+var.add(68, "cerro")
+var.add(24, "vargas")
+var.add(29, "hiperbole")
+print(var.get("culo"))
