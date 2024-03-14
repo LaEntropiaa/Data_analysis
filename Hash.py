1853e776d88c2e413910866aa0846bf52333961f
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-03-14 14:41:35 -0600

    Ok, deleted everything BUT now it works, linear probing is better in this case, i won't make a 10 bazillion items hash table

--- Hash.py
+++ Hash.py
@@ -7,4 +7,26 @@ class Hash:
         self.array = [None for _ in range(self.size)]
         self.items = 0
         
-        
+    def __hash(self, key) -> int:
+        key = hash(key)
+        position = round(self.size * ((key * self.__C) % 1))
+        return position        
+
+    def add(self, data, key) -> None:
+        if self.items >= self.size:
+            raise KeyError("HashTable out of space")
+        position = self.__hash(key)
+        while self.array[position] is not None:
+            if self.array[position][1] == key:
+                self.array[position][0] = data
+                return
+            position = (position + 1) % self.size
+        self.array[position] = (data, key)
+    
+    def get(self, key):
+        position = self.__hash(key)
+        while self.array[position] is not None:
+            if self.array[position][1] == key:
+                return self.array[position][0]
+            position = (position + 1) % self.size
+        raise KeyError("Key not found")
